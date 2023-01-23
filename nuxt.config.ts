511e37d2e86af00bab29c48f24198ac6b58ffafc
ncoffez <ncoffez@gmail.com>
2023-01-23 22:14:23 +0100

    deploying v2 to master

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   typescript: { shim: false },
-  css: ['@/assets/global.sass', '@/assets/theme.scss'],
+  css: ["@/assets/global.sass", "@/assets/theme.scss"],
+  runtimeConfig: {
+    apiKey: process.env.FIREBASE_CONFIG,
+  },
 });

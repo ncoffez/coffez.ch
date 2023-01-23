f72524abbaf17354c1e1e0795913498499266d36
ncoffez <ncoffez@gmail.com>
2023-01-23 21:55:02 +0100

    fixed leaked API Key

--- plugins/firebase.js
+++ plugins/firebase.js
@@ -7,18 +7,10 @@ import { getFirestore} from 'firebase/firestore';
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyAY7PEpKzkcbbswBAmusNNBFpDdGAethE4",
-  authDomain: "coffezv2.firebaseapp.com",
-  projectId: "coffezv2",
-  storageBucket: "coffezv2.appspot.com",
-  messagingSenderId: "949160522929",
-  appId: "1:949160522929:web:50f44aacc7015ca7c45e90",
-  measurementId: "G-QBE95WMX0E"
-};
+;
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const app = initializeApp(process.env.firebaseConfig);
 export const functions = getFunctions();
 export const db = getFirestore();
 

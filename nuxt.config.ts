878aec58e94bc09ebe488eaca1cb078f0509eb64
Nicolas Coffez <ncoffez@gmail.com>
2023-09-12 13:46:06 +0200

    minor fixes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: false },
   runtimeConfig: {
     firebase: {
       apiKey: process.env.NUXT_API_KEY, // can be overridden by NUXT_PUBLIC_API_BASE environment variable

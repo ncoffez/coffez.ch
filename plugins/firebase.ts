b78931119bb1fed63ddaeacdad33e56ca0ac46a4
Nicolas Coffez <ncoffez@gmail.com>
2024-02-17 08:27:23 +0100

    new API key

--- plugins/firebase.ts
+++ plugins/firebase.ts
@@ -4,17 +4,8 @@ import rawServiceAccount from "./serviceAccount.env.json";
 import type { ServiceAccount } from "firebase-admin";
 
 export default defineNuxtPlugin((nuxtApp) => {
-  const firebaseConfig = {
-    apiKey: "AIzaSyBxK3sW3_Htj-4uEz6yetCrWrVybJ3QCcc",
-    authDomain: "coffez-ch.firebaseapp.com",
-    databaseURL: "https://coffez-ch-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "coffez-ch",
-    storageBucket: "coffez-ch.appspot.com",
-    messagingSenderId: "1008451410892",
-    appId: "1:1008451410892:web:c39aea5c1b90dc12501b69",
-    measurementId: "G-LQY4FG2BEB",
-  };
+  const firebaseConfig = rawServiceAccount as ServiceAccount;
   const app = initializeApp(firebaseConfig);
-  
+
   nuxtApp.provide("db", getFirestore(app));
 });

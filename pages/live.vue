9713bb9e7dd7ba0ce8f7ac3c74ae7d0789bae314
Nicolas Coffez <ncoffez@gmail.com>
2024-02-18 23:41:17 +0100

    added media queries and new layout to live page

--- pages/live.vue
+++ pages/live.vue
@@ -1,14 +1,19 @@
 <template>
-<section>
+<section v-if="images.length > 0">
   <h4>{{ settings.title }}</h4>
-  <div class="grid">
-    <div v-for="image in  images" class="image-box">
+  <div class="custom-grid">
+    <div v-for="image in images" class="image-box">
       <nuxtLink :to="'/sales/' + image.id">
         <img :src="image.urlFirebaseWebp" :alt="image.name">
       </nuxtLink>
       <small>{{ intlFormatDistance(image.createdDate.toDate(), now) }}</small>
     </div>
   </div>
+</section>
+
+<section v-else>
+  <h1>Okay you got me</h1>
+  <p>Please tell me where I can find Mr. Chocolate.</p>
 </section>
 </template>
 <script lang='ts' setup>
@@ -19,15 +24,15 @@ import { collection, query, onSnapshot, CollectionReference, Firestore, orderBy,
 const nuxtApp = useNuxtApp();
 const db = nuxtApp.$db as Firestore;
 
-let settings = ref({ title: 'Coffez.ch - Live', startDate: subDays(new Date(), 1.2) })
+let settings: Ref<{ title: string, startDate: Timestamp | Date }> = ref({ title: 'Coffez.ch - Live', startDate: Timestamp.fromDate(subDays(new Date(), 1.2)) })
 let now = ref(new Date());
 
 onMounted(() => setInterval(() => {
   now.value = new Date();
 }, 1000))
 
-const settingsData = (await getDoc(doc(db, 'settings/gallery'))).data();
-if (settingsData) settings.value = { ...settingsData } as any
+const settingsData: { title: string, startDate: Timestamp | Date } = (await getDoc(doc(db, 'settings/gallery'))).data() as any;
+if (settingsData) settings.value = { ...settingsData }
 
 const portraitsRef: CollectionReference = collection(db, "portraits");
 const q = query(portraitsRef, where('createdDate', '>=', settings.value.startDate), orderBy('createdDate', 'desc'), orderBy('urlFirebaseWebp'));
@@ -45,26 +50,81 @@ onUnmounted(unsubscribe);
 </script>
 <style lang='sass' scoped>
 section
-  height: 100vh
-  padding: 3em
+  padding: 2.5rem 2rem
   display: flex
   flex-direction: column
+  overflow: hidden
+  height: 100vh
+
+  @media screen and (max-width: 850px)
+    padding: 1.5em
+
+  @media screen and (max-width: 500px)
+    padding: 1em 2em
+    overflow: scroll
+
+h4
+  margin-block: .5em 1.5em
+
+  @media screen and (max-width: 850px)
+    margin-block: 0 1em
+
+.custom-grid
+  display: grid
+  grid-template: 1fr 1fr / repeat(5, 1fr)
+  gap: 1em 2em
+  width: 100%
   overflow-y: scroll
-.grid
-  grid-template: 1fr 1fr / repeat(auto-fit, minmax(230px, 1fr))
+
+  @media screen and (min-width: 1400px)
+    grid-template: 1fr 1fr 1fr / repeat(7, 1fr)
+    gap: 1em 3em
+
+  @media screen and (max-width: 850px) and (min-width: 701px)
+    grid-template: 1fr 1fr / repeat(4, 1fr)
+    gap: 1em 2em
+
+  @media screen and (max-width: 700px) and (min-width: 501px)
+    grid-template: 1fr 1fr / 1fr 1fr
+
+  @media screen and (max-width: 500px)
+    display: flex
+    flex-direction: column
+    gap: 2em
+    overflow: unset
 
 .image-box
   display: flex
   flex-direction: column
-  justify-content: space-around
+
+  @media screen and (min-width: 701px)
+    &:first-child
+      grid-area: 1 / 1 / 3 / 3
+      height: 100%
+      width: 100%
+      
+      img
+        aspect-ratio: 21/30.3
+        overflow: hidden
+        object-fit: cover
+        height: calc((100vh - 10.65em))
+        width: 100%
 
 img
   aspect-ratio: 21/29.7
+  height: calc((100vh - 13em) / 2)
   object-position: center
+
+  @media screen and (max-width: 500px)
+    width: 100%
+    height: 100%
+    aspect-ratio: unset
+    object-fit: cover
 
 small
   font-size: .9em
   opacity: .5
   width: 100%
   text-align: center
+
 </style>

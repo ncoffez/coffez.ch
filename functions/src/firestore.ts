34d080184c4447da892454087a43f9f1a6ba50c0
Nicolas Coffez <41234413+ncoffez@users.noreply.github.com>
2025-04-27 17:43:33 +0300

    feat/github actions-v3

--- functions/src/firestore.ts
+++ functions/src/firestore.ts
@@ -1,11 +1,16 @@
-import admin from "firebase-admin";
+import admin, { ServiceAccount } from "firebase-admin";
 import { getFirestore } from "firebase-admin/firestore";
+const FIREBASE_SERVICE_ACCOUNT: ServiceAccount = process.env.FIREBASE_SERVICE_ACCOUNT
+  ? JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT)
+  : (() => {
+      throw new Error("FIREBASE_SERVICE_ACCOUNT is not defined in environment variables");
+    })();
 
 admin.initializeApp({
-  credential: admin.credential.cert('./serviceAccount.env.json'),
+  credential: admin.credential.cert(FIREBASE_SERVICE_ACCOUNT),
   databaseURL: "https://coffez-ch-default-rtdb.europe-west1.firebasedatabase.app",
 });
 
 export const db = getFirestore();
-export const bucket = admin.storage().bucket("coffez-ch.appspot.com")
+export const bucket = admin.storage().bucket("coffez-ch.appspot.com");
 export default admin;

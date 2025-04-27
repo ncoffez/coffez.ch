31856e119be68ca2768e19161fdf5803c1722468
Nicolas Coffez <41234413+ncoffez@users.noreply.github.com>
2025-04-27 23:06:37 +0300

    feat/github-actions-deploy-only-functions

--- functions/src/util/dateFromTimestamp.ts
+++ functions/src/util/dateFromTimestamp.ts
@@ -1,4 +1,4 @@
-import { Timestamp } from "firebase/firestore";
+import { Timestamp } from "firebase-admin/firestore";
 
 export default function dateFromTimestamp(timestamp: Timestamp): Date {
   return timestamp.toDate();

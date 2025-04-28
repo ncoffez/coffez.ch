5daba68b1c2b262db3439585f3d6bff6ac89b1d8
Nicolas Coffez <41234413+ncoffez@users.noreply.github.com>
2025-04-28 17:33:55 +0300

    can now commit preview builds

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -19,6 +19,7 @@ export default defineNuxtConfig({
   nitro: {
     preset: "firebase",
     firebase: {
+      serverFunctionName: process.env.ENV === "preview" ? "server-preview" : "server",
       gen: 2,
       httpsOptions: {
         region: "europe-west6",

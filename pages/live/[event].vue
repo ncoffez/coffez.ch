f82d3c5cd9e6b2278a46aefc437a2729514b6a2b
Nicolas Coffez <41234413+ncoffez@users.noreply.github.com>
2025-05-01 08:07:59 +0300

    initial commit

--- pages/live/[event].vue
+++ pages/live/[event].vue
@@ -1,72 +1,76 @@
 <template>
-  <div class="bg-neutral-50">
-    <div id="gallery" class="md:overflow-y-clip sm:h-vh px-8" v-if="images.length > 0">
-      <div class="grid grid-cols-[1fr,auto]">
-        <section id="title" class="w-full p-4 sticky top-0 z-2 stretch-1">
-          <h2 class="hidden md:block text-4xl font-extrabold" id="url-title">
-            <NuxtLink
-              class="dark:text-primary-600 text-primary-800 hover:text-primary-900 dark:hover:text-primary-700 pe-1 leading-normal"
-              to="/"
-              >coffez.ch</NuxtLink
-            >/live
-          </h2>
-          <div id="subtitle" class="flex text-center items-center gap-2">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="18"
-              height="18"
-              viewBox="0 0 24 24"
-              @click="router.push('/')"
-              class="md:hidden w-6 h-6 fill-slate-600 dark:fill-slate-300 dark:text-zinc-500 dark:hover:text-zinc-300 cursor-pointer transition-colors duration-200">
-              <!-- Icon from Google Material Icons by Material Design Authors - https://github.com/material-icons/material-icons/blob/master/LICENSE -->
-              <path
-                d="M10 19v-5h4v5c0 .55.45 1 1 1h3c.55 0 1-.45 1-1v-7h1.7c.46 0 .68-.57.33-.87L12.67 3.6c-.38-.34-.96-.34-1.34 0l-8.36 7.53c-.34.3-.13.87.33.87H5v7c0 .55.45 1 1 1h3c.55 0 1-.45 1-1" />
-            </svg>
-            <h4 class="text-3xl font-light md:text-xl md:leading-tight" @click="">{{ settings?.title }}</h4>
-          </div>
-        </section>
-        <div class="grid items-center">
-          <UiDownloadAllImages :images="filteredImages" :title="settings?.title" v-if="route.query.download">
-            <Icon name="ic:outline-save-alt" class="text-lg"></Icon>
-          </UiDownloadAllImages>
-        </div>
-      </div>
-      <section
-        id="images"
-        class="grid sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-y-4 gap-x-6"
-        ref="container">
-        <div
-          :key="image.id"
-          :class="index === 0 ? 'sm:col-span-2 sm:row-span-2 h-full flex flex-col' : ''"
-          v-for="(image, index) in filteredImages">
-          <nuxtLink :to="'/sales/' + image.id">
-            <img
-              :alt="image.name"
-              :src="image.index === 0 ? image.urlFirebaseOriginal : image.urlFirebaseWebp"
-              class="object-cover rounded-lg w-full flex-grow shadow-md dark:shadow-none" />
-          </nuxtLink>
-          <p class="text-sm dark:text-slate-400 font-base text-center leading-relaxed pt-1">
-            {{ toRelativeDate(image.createdDate.toDate()) }}
-          </p>
-        </div>
-      </section>
-    </div>
+	<div class="bg-neutral-50">
+		<div id="gallery" class="md:overflow-y-clip sm:h-vh px-8" v-if="images.length > 0">
+			<div class="grid grid-cols-[1fr,auto]">
+				<section id="title" class="w-full p-4 sticky top-0 z-2 stretch-1">
+					<h2 class="hidden md:block text-4xl font-extrabold" id="url-title">
+						<NuxtLink
+							class="dark:text-primary-600 text-primary-800 hover:text-primary-900 dark:hover:text-primary-700 pe-1 leading-normal"
+							to="/"
+							>coffez.ch</NuxtLink
+						>/live
+					</h2>
+					<div id="subtitle" class="flex text-center items-center gap-2">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							width="18"
+							height="18"
+							viewBox="0 0 24 24"
+							@click="router.push('/')"
+							class="md:hidden w-6 h-6 fill-slate-600 dark:fill-slate-300 dark:text-zinc-500 dark:hover:text-zinc-300 cursor-pointer transition-colors duration-200">
+							<!-- Icon from Google Material Icons by Material Design Authors - https://github.com/material-icons/material-icons/blob/master/LICENSE -->
+							<path
+								d="M10 19v-5h4v5c0 .55.45 1 1 1h3c.55 0 1-.45 1-1v-7h1.7c.46 0 .68-.57.33-.87L12.67 3.6c-.38-.34-.96-.34-1.34 0l-8.36 7.53c-.34.3-.13.87.33.87H5v7c0 .55.45 1 1 1h3c.55 0 1-.45 1-1" />
+						</svg>
+						<h4 class="text-3xl font-light md:text-xl md:leading-tight" @click="">{{ settings?.title }}</h4>
+					</div>
+				</section>
+				<div class="grid items-center">
+					<UiDownloadAllImages :images="filteredImages" :title="settings?.title" v-if="route.query.download">
+						<Icon name="ic:outline-save-alt" class="text-lg"></Icon>
+					</UiDownloadAllImages>
+				</div>
+			</div>
+			<section
+				id="images"
+				class="grid sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-y-4 gap-x-6"
+				ref="container">
+				<div
+					:key="image.id"
+					:class="index === 0 ? 'sm:col-span-2 sm:row-span-2 h-full flex flex-col' : ''"
+					v-for="(image, index) in filteredImages">
+					<nuxtLink :to="'/sales/' + image.id">
+						<img
+							:alt="image.name"
+							:src="image.index === 0 ? image.urlFirebaseOriginal : image.urlFirebaseWebp"
+							class="object-cover rounded-lg w-full flex-grow shadow-md dark:shadow-none" />
+					</nuxtLink>
+					<p class="text-sm dark:text-slate-400 font-base text-center leading-relaxed pt-1">
+						{{ toRelativeDate(image.createdDate.toDate()) }}
+					</p>
+				</div>
+			</section>
+		</div>
 
-    <section id="placeholder" class="h-full justify-between flex flex-col" v-else>
-      <div id="up-next" class="ml-auto block dark:bg-pink-600 text-white text-2xl font-light p-4">
-        {{ settings?.title }}
-      </div>
-      <UiComingSoon class="place-self-center h-full my-auto" />
-    </section>
-  </div>
+		<section id="placeholder" class="h-full justify-between flex flex-col" v-else>
+			<div id="up-next" class="ml-auto block dark:bg-pink-600 text-white text-2xl font-light p-4">
+				{{ settings?.title }}
+			</div>
+			<UiComingSoon class="place-self-center h-full my-auto" />
+		</section>
+	</div>
 </template>
 <script lang="ts" setup>
 import { toRelativeDate } from "#imports";
 import { addDays, subDays } from "date-fns";
 import { collection, query, onSnapshot, CollectionReference, orderBy, where } from "firebase/firestore";
 
-const { $db } = useNuxtApp();
+const { $db, $analytics } = useNuxtApp();
 const db = await $db();
+
+onMounted(() => {
+	$analytics("Event page was visited.");
+});
 
 const { event } = useRoute().params;
 const { data: settings } = await useFetch(`/api/getEvent/${event}`);
@@ -75,27 +79,27 @@ const router = useRouter();
 
 const portraitsRef: CollectionReference = collection(db, "portraits");
 const q = query(
-  portraitsRef,
-  where("createdDate", ">=", new Date(settings.value?.startDate || subDays(new Date(), 1))),
-  where("createdDate", "<=", new Date(settings.value?.endDate || addDays(new Date(), 5))),
-  orderBy("createdDate", "desc"),
-  orderBy("urlFirebaseWebp")
+	portraitsRef,
+	where("createdDate", ">=", new Date(settings.value?.startDate || subDays(new Date(), 1))),
+	where("createdDate", "<=", new Date(settings.value?.endDate || addDays(new Date(), 5))),
+	orderBy("createdDate", "desc"),
+	orderBy("urlFirebaseWebp")
 );
 const images: Ref<any[]> = ref([]);
 
 const unsubscribe = onSnapshot(q, (querySnapshot) => {
-  querySnapshot.docChanges().forEach((change) => {
-    const data = { id: change.doc.id, ...change.doc.data(), index: change.newIndex };
-    if (change.type === "modified") {
-      const index = images.value.findIndex((image) => image.id === data.id);
-      if (index !== -1) {
-        images.value.splice(index, 1, data);
-      } else images.value.splice(change.newIndex, 0, data);
-    }
-    if (change.type === "added") {
-      images.value.splice(data.index, 0, data);
-    }
-  });
+	querySnapshot.docChanges().forEach((change) => {
+		const data = { id: change.doc.id, ...change.doc.data(), index: change.newIndex };
+		if (change.type === "modified") {
+			const index = images.value.findIndex((image) => image.id === data.id);
+			if (index !== -1) {
+				images.value.splice(index, 1, data);
+			} else images.value.splice(change.newIndex, 0, data);
+		}
+		if (change.type === "added") {
+			images.value.splice(data.index, 0, data);
+		}
+	});
 });
 
 onUnmounted(unsubscribe);
@@ -103,49 +107,49 @@ onUnmounted(unsubscribe);
 const filteredImages = computed(() => images.value.filter((image) => !image.hidden));
 
 let meta = {
-  title: settings.value?.title,
-  description: settings.value?.description,
-  coverImage: settings.value?.coverImage,
-  defaultImage: `https://storage.googleapis.com/coffez-ch/analoge_zeichnung.jpeg`,
-  url: `https://coffez.ch/live/${event}`,
-  start: {
-    date: new Date(settings.value?.startDate).toLocaleDateString(),
-    valid: !isNaN(new Date(settings.value?.startDate).getTime()),
-  },
-  end: {
-    date: new Date(settings.value?.endDate).toLocaleDateString(),
-    valid: !isNaN(new Date(settings.value?.endDate).getTime()),
-  },
+	title: settings.value?.title,
+	description: settings.value?.description,
+	coverImage: settings.value?.coverImage,
+	defaultImage: `https://storage.googleapis.com/coffez-ch/analoge_zeichnung.jpeg`,
+	url: `https://coffez.ch/live/${event}`,
+	start: {
+		date: new Date(settings.value?.startDate).toLocaleDateString(),
+		valid: !isNaN(new Date(settings.value?.startDate).getTime()),
+	},
+	end: {
+		date: new Date(settings.value?.endDate).toLocaleDateString(),
+		valid: !isNaN(new Date(settings.value?.endDate).getTime()),
+	},
 };
 
 if (meta.start.valid && meta.end.valid && !meta.description)
-  meta.description += ` Gallery of caricatures drawn from ${meta.start.date} to ${meta.end.date}.`;
+	meta.description += ` Gallery of caricatures drawn from ${meta.start.date} to ${meta.end.date}.`;
 if (meta.start.valid && !meta.end.valid && !meta.description)
-  meta.description += ` Join the live event that started on ${meta.start.date}.`;
+	meta.description += ` Join the live event that started on ${meta.start.date}.`;
 
 useSeoMeta({
-  description: meta.description,
-  ogTitle: meta.title,
-  ogDescription: meta.description,
-  ogImage: meta.coverImage || meta.defaultImage,
-  ogUrl: `https://coffez.ch/live/${event}`,
-  twitterTitle: meta.title,
-  twitterDescription: meta.description,
-  twitterImage: meta.coverImage || meta.defaultImage,
-  twitterCard: "summary",
+	description: meta.description,
+	ogTitle: meta.title,
+	ogDescription: meta.description,
+	ogImage: meta.coverImage || meta.defaultImage,
+	ogUrl: `https://coffez.ch/live/${event}`,
+	twitterTitle: meta.title,
+	twitterDescription: meta.description,
+	twitterImage: meta.coverImage || meta.defaultImage,
+	twitterCard: "summary",
 });
 
 useHead({
-  htmlAttrs: {
-    lang: "en",
-  },
-  link: [
-    {
-      rel: "icon",
-      type: "image/png",
-      href: "/favicon.png",
-    },
-  ],
-  title: meta.title,
+	htmlAttrs: {
+		lang: "en",
+	},
+	link: [
+		{
+			rel: "icon",
+			type: "image/png",
+			href: "/favicon.png",
+		},
+	],
+	title: meta.title,
 });
 </script>
--- pages/sales/[id].vue
+++ pages/sales/[id].vue
@@ -1,91 +1,103 @@
 <template>
-  <div class="w-fit flex flex-col lg:grid lg:grid-cols-[auto,1fr] gap-x-12 gap-y-6 p-8 max-w-screen-lg mx-auto"
-    v-if="image">
-    <div id="text">
-      <div id="toGallery"
-        class="grid grid-cols-[auto,1fr] items-center w-fit text-zinc-500 mb-4 hover:text-zinc-300 cursor-pointer transition-colors duration-200"
-        @click="router.replace(!event ? '/live' : `/live/${event.id}`)">
-        <Icon name="ic:round-chevron-left" class="w-8 h-8" />
-        <div>Back to gallery</div>
-      </div>
-      <hgroup>
-        <h1 class="text-3xl font-bold lg:max-w-96">{{ event?.title || image.originalName }}</h1>
-        <p class="text-lg leading-loose text-zinc-500 gap-3 flex items-center">
-          {{ toLocaleDateTime(image.createdDate) }}
-        </p>
-        <div class="inline-block text-base font-light px-2 py-1 bg-zinc-700 text-zinc-300 rounded-xl my-2">{{
-          image.size }}</div>
-
-      </hgroup>
-    </div>
-    <div id="image" class="max-w-[500px]">
-      <img v-if="image.urlFirebaseOriginalWithQR" :src="image.urlFirebaseOriginalWithQR" alt="" srcset="" />
-      <img v-else :src="image.urlFirebaseReduced" alt="" srcset="" />
-    </div>
-  </div>
+	<div
+		class="w-fit flex flex-col lg:grid lg:grid-cols-[auto,1fr] gap-x-12 gap-y-6 p-8 max-w-screen-lg mx-auto"
+		v-if="image">
+		<div id="text">
+			<div
+				id="toGallery"
+				class="grid grid-cols-[auto,1fr] items-center w-fit text-zinc-500 mb-4 hover:text-zinc-300 cursor-pointer transition-colors duration-200"
+				@click="router.replace(!event ? '/live' : `/live/${event.id}`)">
+				<Icon name="ic:round-chevron-left" class="w-8 h-8" />
+				<div>Back to gallery</div>
+			</div>
+			<hgroup>
+				<h1 class="text-3xl font-bold lg:max-w-96">{{ event?.title || image.originalName }}</h1>
+				<p class="text-lg leading-loose text-zinc-500 gap-3 flex items-center">
+					{{ toLocaleDateTime(image.createdDate) }}
+				</p>
+				<div class="inline-block text-base font-light px-2 py-1 bg-zinc-700 text-zinc-300 rounded-xl my-2">
+					{{ image.size }}
+				</div>
+			</hgroup>
+		</div>
+		<div id="image" class="max-w-[500px]">
+			<img v-if="image.urlFirebaseOriginalWithQR" :src="image.urlFirebaseOriginalWithQR" alt="" srcset="" />
+			<img v-else :src="image.urlFirebaseReduced" alt="" srcset="" />
+		</div>
+	</div>
 </template>
 
 <script setup lang="ts">
-import { Timestamp } from 'firebase/firestore';
+import { Timestamp } from "firebase/firestore";
 const route = useRoute();
 const router = useRouter();
 const baseURL = useRequestURL();
-const currentURL = `${baseURL}${route.fullPath}`
+const currentURL = `${baseURL}${route.fullPath}`;
+const { $analytics } = useNuxtApp();
+
+onMounted(() => {
+	$analytics("User viewed his picture.");
+});
 
 const { data: image } = await useFetch(`/api/getPortrait/${route.params.id}`, {
-  transform: (data: any) => {
-    return {
-      ...data,
-      createdDate: new Timestamp(data.createdDate._seconds, data.createdDate._nanoseconds).toDate(),
-    }
-  }
-})
+	transform: (data: any) => {
+		return {
+			...data,
+			createdDate: new Timestamp(data.createdDate._seconds, data.createdDate._nanoseconds).toDate(),
+		};
+	},
+});
 
-const { data: event } = await useLazyFetch<any>('/api/getEvent', {
-  method: 'POST',
-  headers: { 'Content-Type': 'application/json' },
-  body: { date: new Date(image.value.createdDate).toISOString() }
+const { data: event } = await useLazyFetch<any>("/api/getEvent", {
+	method: "POST",
+	headers: { "Content-Type": "application/json" },
+	body: { date: new Date(image.value.createdDate).toISOString() },
 });
 
 function toLocaleDateTime(date: Date | null) {
-  if (!date) return 'Date is not valid.';
-  return date.toLocaleDateString('de-CH', { weekday: 'long', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit' })
+	if (!date) return "Date is not valid.";
+	return date.toLocaleDateString("de-CH", {
+		weekday: "long",
+		month: "long",
+		day: "numeric",
+		hour: "2-digit",
+		minute: "2-digit",
+	});
 }
 
 let meta = {
-  title: event.value?.title,
-  description: event.value?.description,
-  coverImage: image.value?.urlFirebaseJpg,
-  defaultImage: `https://storage.googleapis.com/coffez-ch/analoge_zeichnung.jpeg`,
-  url: currentURL,
-}
+	title: event.value?.title,
+	description: event.value?.description,
+	coverImage: image.value?.urlFirebaseJpg,
+	defaultImage: `https://storage.googleapis.com/coffez-ch/analoge_zeichnung.jpeg`,
+	url: currentURL,
+};
 
 useSeoMeta({
-  description: meta.description,
-  ogTitle: meta.title,
-  ogDescription: meta.description,
-  ogImage: meta.coverImage || meta.defaultImage,
-  ogUrl: currentURL,
-  twitterTitle: meta.title,
-  twitterDescription: meta.description,
-  twitterImage: meta.coverImage || meta.defaultImage,
-  twitterCard: 'summary'
-})
+	description: meta.description,
+	ogTitle: meta.title,
+	ogDescription: meta.description,
+	ogImage: meta.coverImage || meta.defaultImage,
+	ogUrl: currentURL,
+	twitterTitle: meta.title,
+	twitterDescription: meta.description,
+	twitterImage: meta.coverImage || meta.defaultImage,
+	twitterCard: "summary",
+});
 
 useHead({
-  htmlAttrs: {
-    lang: 'en'
-  },
-  link: [
-    {
-      rel: 'icon',
-      type: 'image/png',
-      href: '/favicon.png'
-    }
-  ],
-  title: meta.title,
-})
-
+	htmlAttrs: {
+		lang: "en",
+	},
+	link: [
+		{
+			rel: "icon",
+			type: "image/png",
+			href: "/favicon.png",
+		},
+	],
+	title: meta.title,
+});
 </script>
 
 <style scoped></style>

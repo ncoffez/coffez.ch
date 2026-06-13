b5f5351d13abd76b36252ad963a025e89b3cdc61
PascalCoffez <85305285+PascalCoffez@users.noreply.github.com>
2026-06-13 06:23:55 +0200

    Add files via upload

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,10 @@
 <template>
 	<Header></Header>
 	<Brands />
+	<Presentation />
+	<section class="w-full max-w-screen-lg mx-auto px-6 pt-16 pb-2 text-center">
+		<h2 class="text-3xl md:text-4xl font-bold dark:text-slate-100 text-stone-900">{{ $t('galleriesTitle') }}</h2>
+	</section>
 	<LazyGallery />
 	<LazyCards />
 	<Contact />
